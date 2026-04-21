b2a834522c80da8456b133fd390b19c5fb9c665a
leastinformednerd <automagicwyvern@gmail.com>
2026-04-21 12:45:50 +1200

    Changed handling of environment setup Moved some env vars out of ~/.zshrc into the global nix config, making them globally visible and available on startup

--- configuration.nix
+++ configuration.nix
@@ -141,15 +141,23 @@ with {
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    alacritty
-    pipewire
-    pulseaudio
-    pavucontrol
-    fuzzel
-    swaylock
+  environment = {
+    systemPackages = with pkgs; [
+      helix
+      alacritty
+      pipewire
+      pulseaudio
+      pavucontrol
+      fuzzel
+      swaylock
    ];
+   variables = {
+     PATH = [
+       "${config.users.users.myself.home}/.local/scripts"
+     ];
+     EDITOR = "${pkgs.helix}/bin/hx";
+   };
+  };
 
   programs = {
     steam.enable = true;
